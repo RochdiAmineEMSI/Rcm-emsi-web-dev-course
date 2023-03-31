c88c39e879fc40d4836ccf2bdd0b0359c636206b
Rocmine <amine.rochdi@emsi-edu.ma>
2023-03-31 12:22:03 +0000

    coursera

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,6 @@
 (function () {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Alice", "Jesse", "Charlie", "Jake", "Eva", "Felix", "Grace", "Henry", "Isabella", "Jack"];
+
 
 for (var name in names) {
 
